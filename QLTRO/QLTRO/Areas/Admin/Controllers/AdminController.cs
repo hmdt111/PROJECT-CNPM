c1aaadc62a48623da9655bc15264ee084978c379
hmdt111 <huy8x11@gmail.com>
2024-06-19 01:32:34 +0700

    git init

---
+++ QLTRO/QLTRO/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using QLTRO.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLTRO.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        QLnhatroEntities db = new QLnhatroEntities();
+        // GET: Admin/Admin
+        public ActionResult HoaDon()
+        {
+            List<HOADON> hoadon = db.HOADONs.ToList();
+            return View(hoadon);
+        }
+
+
+
+        public ActionResult HopDong()
+        {
+            List<HOPDONG> hd = db.HOPDONGs.ToList();
+            return View(hd);
+        }
+
+        public ActionResult NguoiDung()
+        {
+            List<NGUOIDUNG> nd = db.NGUOIDUNGs.ToList();
+            return View(nd);
+        }
+
+        public ActionResult AddNguoiDung()
+        {
+            
+            return View();
+        }
+        public ActionResult PhongTro()
+        {
+            List<PHONGTRO> pt = db.PHONGTROes.ToList();
+            return View(pt);
+        }
+    }
+}
